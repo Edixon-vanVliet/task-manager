2342ae311b1c7751a547966d20381db14ebb00c8
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-11-13 10:46:48 -0400

    Create task list component

---
+++ client/src/components/task-list/TaskList.js
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Card } from "../card/Card";
+
+import styles from "./TaskList.module.css";
+
+export const TaskList = () => (
+  <Card>
+    <div className={styles.task}>
+      <p className={styles.title}>Walk The Cat</p>
+      <div className={styles.controls}>
+        <FontAwesomeIcon className={styles.button} title="edit" icon={faEdit} color="#2c5336" />
+        <FontAwesomeIcon className={styles.button} title="remove" icon={faTrash} color="#6d021d" />
+      </div>
+    </div>
+  </Card>
+);
