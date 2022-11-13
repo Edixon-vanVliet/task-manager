a4af9ae2a444ca2bf77d1c413cdc6f6a7ba612ea
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-11-13 09:12:05 -0400

    Add package React-Router-DOM

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,9 @@
-export const App = () => <h1>Task Manager</h1>;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, EditTask } from "./pages";
+
+export const App = () => (
+  <Router>
+    <Routes>
+    </Routes>
+  </Router>
+);
