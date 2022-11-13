a4bcc102fb1592006073103c045ea029920c3d3f
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-11-13 11:36:39 -0400

    Add navigation

--- client/src/components/task-list/TaskList.js
+++ client/src/components/task-list/TaskList.js
@@ -1,17 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Card } from "../card/Card";
 
 import styles from "./TaskList.module.css";
 
-export const TaskList = () => (
-  <Card>
-    <div className={styles.task}>
-      <p>Walk The Cat</p>
-      <div className={styles.controls}>
-        <FontAwesomeIcon className={styles.button} title="edit" icon={faEdit} color="#2c5336" />
-        <FontAwesomeIcon className={styles.button} title="remove" icon={faTrash} color="#6d021d" />
+export const TaskList = () => {
+  const navigate = useNavigate();
+
+  const handleEdit = (id) => {
+    navigate(`/task/${id}`);
+  };
+
+  return (
+    <Card>
+      <div className={styles.task}>
+        <p>Walk The Cat</p>
+        <div className={styles.controls}>
+          <FontAwesomeIcon
+            className={styles.button}
+            title="edit"
+            icon={faEdit}
+            color="#2c5336"
+            onClick={() => handleEdit(1)}
+          />
+          <FontAwesomeIcon className={styles.button} title="remove" icon={faTrash} color="#6d021d" />
+        </div>
       </div>
-    </div>
-  </Card>
-);
+    </Card>
+  );
+};
