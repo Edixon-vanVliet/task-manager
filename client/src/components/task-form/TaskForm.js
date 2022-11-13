a9e912fdb7b5a999fa99a4a9386a3b84990fee16
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-11-13 10:46:04 -0400

    Create task form component

---
+++ client/src/components/task-form/TaskForm.js
@@ -0,0 +1,14 @@
+import { Card } from "../card/Card";
+import styles from "./TaskForm.module.css";
+
+export const TaskForm = () => (
+  <Card>
+    <form className={styles.form}>
+      <caption className={styles.title}>Task Manager</caption>
+      <div className={styles.controls}>
+        <input className={styles.input} type="text" placeholder="e.g. wash dishes" />
+        <button className={styles.button}>Submit</button>
+      </div>
+    </form>
+  </Card>
+);
