4c9e3d78a8c3ca6be81087596d254c6ebb955e7e
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-11-13 20:00:44 -0400

    Return more detailed responses

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -1,8 +1,8 @@
-let tasks = [];
-let currentId = 0;
+let tasks = [{ id: 1, name: "Pepe", completed: false }];
+let currentId = 1;
 
 export const getTasks = (req, res) => {
-  res.json({ count: tasks.length, data: tasks });
+  res.json({ count: tasks.length, data: tasks, success: true });
 };
 
 export const getTask = (req, res) => {
@@ -10,10 +10,10 @@ export const getTask = (req, res) => {
   const task = tasks.find((task) => task.id === id);
 
   if (!task) {
-    return res.status(404).json({ message: "Task not found" });
+    return res.status(404).json({ message: "Task not found", success: false });
   }
 
-  res.json(task);
+  res.json({ data: task, success: true });
 };
 
 export const addTask = (req, res) => {
@@ -21,9 +21,12 @@ export const addTask = (req, res) => {
 
   try {
     tasks.push(task);
-    res.status(201).location(`/tasks/${task.id}`).json({ message: "Task created successfully" });
+    res
+      .status(201)
+      .location(`/tasks/${task.id}`)
+      .json({ message: "Task created successfully", data: task, success: true });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: err.message, success: false });
   }
 };
 
@@ -36,7 +39,7 @@ export const updateTask = (req, res) => {
   };
 
   if (!tasks.find((task) => task.id === id)) {
-    return res.status(404).json({ message: "Task not found" });
+    return res.status(404).json({ message: "Task not found", success: false });
   }
 
   tasks = tasks.map((task) => {
@@ -47,17 +50,17 @@ export const updateTask = (req, res) => {
     return task;
   });
 
-  res.json(updatedTask);
+  res.json({ message: "Task updated successfully", data: updatedTask, success: true });
 };
 
 export const removeTask = (req, res) => {
   const id = parseInt(req.params.id);
 
   if (!tasks.find((task) => task.id === id)) {
-    return res.status(404).json({ message: "Task not found" });
+    return res.status(404).json({ message: "Task not found", success: false });
   }
 
   tasks = tasks.filter((task) => task.id !== id);
 
-  res.status(204).json();
+  res.status(200).json({ success: true, data: id, message: "Task deleted successfully" });
 };
